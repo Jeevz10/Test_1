8450267b1cea061f6766d0dfe6715decaf8e4213
Jeevz10 <theghosthaunter@hotmail.com>
2018-06-27 18:48:19 +0800

    Initial commit

---
+++ Assets/AddForce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour {
+    Rigidbody2D rb;
+    public float force = 1500f;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        float h = Input.GetAxis("Horizontal") * force * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * force * Time.deltaTime;
+
+        /*
+        rb.AddForce(new Vector2(h,0));
+        rb.AddForce(new Vector2(0,v));
+        */
+
+        rb.AddTorque(v);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed = 1f;
+    public float torque = 5f;
+    public float amount = 50f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+
+    // Update is called once per frame
+    
+	void Update () {
+
+        Vector2 dir = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            dir += new Vector2(1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            dir += Vector2.down;
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            dir += Vector2.right;
+        }
+        dir = dir.normalized;
+
+        Vector2 translateDist = dir * Speed * Time.deltaTime;
+        transform.Translate(translateDist);
+	}
+
+
+  
+}
